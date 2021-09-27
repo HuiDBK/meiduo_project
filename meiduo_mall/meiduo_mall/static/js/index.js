ea4bd552b1bdaabca59d21719eef512b949a69f2
hui <2228157440@qq.com>
2021-09-27 22:20:12 +0800

    利用django自带的LoginRequiredMixin完成用户权限的控制

--- meiduo_mall/meiduo_mall/static/js/index.js
+++ meiduo_mall/meiduo_mall/static/js/index.js
@@ -13,7 +13,7 @@ let vm = new Vue({
     },
     methods: {
         // 获取简单购物车数据
-        get_carts(){
+        get_carts() {
             let url = '/carts/simple/';
             axios.get(url, {
                 responseType: 'json',
@@ -21,8 +21,8 @@ let vm = new Vue({
                 .then(response => {
                     this.carts = response.data.cart_skus;
                     this.cart_total_count = 0;
-                    for(let i=0;i<this.carts.length;i++){
-                        if (this.carts[i].name.length>25){
+                    for (let i = 0; i < this.carts.length; i++) {
+                        if (this.carts[i].name.length > 25) {
                             this.carts[i].name = this.carts[i].name.substring(0, 25) + '...';
                         }
                         this.cart_total_count += this.carts[i].count;
