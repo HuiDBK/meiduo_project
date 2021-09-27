0196b836708a0ab483a3d685c20000a7eb5ec72c
hui <2228157440@qq.com>
2021-09-27 21:32:34 +0800

    通过把用户名保存在cookie中实现vue渲染首页用户名展示

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^register$', views.RegisterView.as_view(), name='register'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
 ]
