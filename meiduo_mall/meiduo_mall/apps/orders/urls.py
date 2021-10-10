fb1b1d28a1681b0c1f4e6e90d1ee6f49732fbc4b
hui <2228157440@qq.com>
2021-10-10 12:57:23 +0800

    新增订单模块

---
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Hui
+# @Desc: { 订单模块路由配置 }
+# @Date: 2021/10/10 12:53
+
+
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^$'),
+]
