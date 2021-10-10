c686116159d0cf6feff32de590bbec74d897cee4
hui <2228157440@qq.com>
2021-10-10 15:36:40 +0800

    新增订单模块html模板文件

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from meiduo_mall.utils.views import LoginRequiredMixin
 
-# Create your views here.
+
+class OrderSettlementView(LoginRequiredMixin, View):
+    """结算订单"""
+
+    def get(self, request):
+        """提供订单结算页面"""
+        return render(request, 'orders/place_order.html')
