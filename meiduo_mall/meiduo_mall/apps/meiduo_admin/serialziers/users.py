02cdb47b522d3206bb160a7d3f23f3b398c37a46
hui <2228157440@qq.com>
2021-10-14 22:08:32 +0800

    完成后台新增用户功能

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serialziers/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serialziers/users.py
@@ -1,12 +1,12 @@
-from rest_framework import serializers
-
-from users.models import User
 import re
 
+from users.models import User
+from rest_framework import serializers
 
-class UserSerialzier(serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     """
-        用户序列化器
+    用户序列化器
     """
 
     class Meta:
@@ -37,5 +37,31 @@ class UserSerialzier(serializers.ModelSerializer):
         # user.set_password(validated_data['password'])
         # user.save()
 
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserAddSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'mobile', 'email', 'password')
+        # username字段增加长度限制，password字段只参与保存，不在返回给前端，增加write_only选项参数
+        extra_kwargs = {
+            'username': {
+                'max_length': 20,
+                'min_length': 5
+            },
+            'password': {
+                'max_length': 20,
+                'min_length': 8,
+                'write_only': True
+
+            },
+        }
+
+    # 重写create方法
+    def create(self, validated_data):
+        # 保存用户数据并对密码加密
         user = User.objects.create_user(**validated_data)
         return user
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView
 
-from meiduo_admin.serialziers.users import UserSerialzier
+from meiduo_admin.serialziers.users import UserSerializer
+from meiduo_admin.serialziers.users import UserAddSerializer
 from meiduo_admin.utils import UserPageNum
 from users.models import User
 
@@ -12,9 +13,19 @@ class UserView(ListCreateAPIView):
     """
 
     # 指定序列化器
-    serializer_class = UserSerialzier
+    serializer_class = UserSerializer
+
     # 使用分页器
     pagination_class = UserPageNum
+
+    # 根据不同的请求方式返回不同序列化器
+    def get_serializer_class(self):
+        # 请求方式是GET，则是获取用户数据返回UserSerializer
+        if self.request.method == 'GET':
+            return UserSerializer
+        else:
+            # POST请求，完成保存用户，返回UserAddSerializer
+            return UserAddSerializer
 
     # 重写获取查询集数据的方法
     def get_queryset(self):
